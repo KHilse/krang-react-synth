f25d02372bcbf803826ad1a9ad59e30578757106
stanstaton <thatkidenzo@gmail.com>
2019-10-03 16:03:48 -0700

    ?

--- src/components/drum machine/frame.js
+++ src/components/drum machine/frame.js
@@ -3,10 +3,13 @@ import styled from "styled-components";
 
 const Frame = styled.div`
   display: grid;
+  margin: 50px;
   grid-template-columns: repeat(${props => props.columns}, 1fr);
   grid-template-rows: repeat(${props => props.rows}, 1fr);
-  width: 100vw;
+  width: 80vw;
   height: calc(70vh - 60px);
+  
+  position: relative;
 `;
 
 export default Frame;
