a3e658311762e7d75eec85fdf463213dd3696646
Kirk Hilse <khilsedev@gmail.com>
2019-10-03 10:06:50 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>Bebop and Rock Steady present...</h2>
+        <h1>Krang, a react band in a page</h1>
       </header>
     </div>
   );
