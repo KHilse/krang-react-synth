5d91f9e9e16af8d025b7c6be25c44dab34c8eb8d
stanstaton <thatkidenzo@gmail.com>
2019-10-03 16:40:38 -0700

    color scheme

--- src/components/drum machine/frame.js
+++ src/components/drum machine/frame.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Frame = styled.div`
   display: grid;
-  margin: 10px;
+  margin-left: 50px;
   grid-template-columns: repeat(${props => props.columns}, 1fr);
   grid-template-rows: repeat(${props => props.rows}, 1fr);
   width: 60vw;
