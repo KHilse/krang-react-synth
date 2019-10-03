49648a62595a5ed01ed5ceef49267b14f2993ee8
stanstaton <thatkidenzo@gmail.com>
2019-10-03 16:15:34 -0700

    drum sequence fits in the container

--- src/components/drum machine/frame.js
+++ src/components/drum machine/frame.js
@@ -3,13 +3,13 @@ import styled from "styled-components";
 
 const Frame = styled.div`
   display: grid;
-  margin: 50px;
+  margin: 10px;
   grid-template-columns: repeat(${props => props.columns}, 1fr);
   grid-template-rows: repeat(${props => props.rows}, 1fr);
-  width: 80vw;
+  width: 60vw;
   height: calc(70vh - 60px);
   
-  position: relative;
+  
 `;
 
 export default Frame;
