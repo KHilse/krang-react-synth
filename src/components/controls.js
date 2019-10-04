44aa519f5e638200258b34f125bf25da638bd5d8
stanstaton <thatkidenzo@gmail.com>
2019-10-04 09:05:07 -0700

    css stuff

--- src/components/controls.js
+++ src/components/controls.js
@@ -4,6 +4,9 @@ const Controls = props => {
     return (
         <div className="controls-container">
             <p>Controls placeholder</p>
+            <div>
+                <input type="range" min="1" max="100" value="50" className="slider" />
+            </div>
         </div>
     )
 }
